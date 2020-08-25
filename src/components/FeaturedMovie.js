c50baf88a11b8771e0d3d30ad91a48841898d456
Gustavo Raeski <gustavoraeski@outlook.com>
2020-08-25 14:42:43 -0300

    estilizando featured e adicionando informações para ela

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,8 +3,19 @@ import './FeaturedMovie.css'
 
 export default ({item}) => {
   return (
-    <section className="featured">
-      <div>{item.original_name}</div>
+    <section className="featured" style={{
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+    }}>
+      <div className="featured-vertical">
+        <div className="featured-horizontal">
+          <div className="featured--name">{item.original_name}</div>
+          <div className="featured--info">
+            <div className="featured--points">{item.vote_average}pontos</div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
