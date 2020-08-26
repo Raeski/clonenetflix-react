078b0f2ac5cd4280843d1f1b1547fca21fb11789
Gustavo Raeski <gustavoraeski@outlook.com>
2020-08-26 09:58:17 -0300

    Adicionando seta de navegação

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,11 +1,20 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import './MovieRow.css';
 
 export default ({title, items}) => {
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow--left">
+        <NavigateBeforeIcon style={{fontSize: 50}} />
+      </div>
+      <div className="movieRow--left">
+        <NavigateNextIcon style={{fontSize: 50}} />
+      </div>
+
       <div className="movieRow--listarea">
         <div className="movieRow--list">
           {items.results.length > 0 && items.results.map((item,key) =>(
