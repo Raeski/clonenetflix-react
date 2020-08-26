810b3af448f8a44eb3ba7ec83bb075ddeb186b55
Gustavo Raeski <gustavoraeski@outlook.com>
2020-08-26 13:24:39 -0300

    Ajustes finais

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -11,8 +11,8 @@ export default ({item}) => {
   }
 
   let description = item.overview;
-  if(description.lenght > 200) {
-    description = description.substring(0,200)+'...';
+  if(description.length > 200) {
+    description = description.substring(0, 200)+'...';
   }
 
   return (
@@ -29,7 +29,7 @@ export default ({item}) => {
             <div className="featured--year">{firstDate.getFullYear()}</div>
             <div className="featured--seasons">{item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
           </div>
-          <div className="featured--description">{item.overview}</div>
+          <div className="featured--description">{description}</div>
           <div className="featured--buttons">
             <a href={`/watch/${item.id}`} className="featured--watchbutton"> ► Assistir</a>
             <a href={`/list/add/${item.id}`}  className="featured--mylistbutton">+ Minha Lista</a>
